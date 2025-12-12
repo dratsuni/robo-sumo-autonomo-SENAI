688ee0cf291210ac4f9c1010f8c19a310d8d76bc
dratsuni <matherthentic@gmail.com>
2025-12-12 16:16:32 -0300

    primeiro commit

---
+++ robo-sumo.ino
@@ -0,0 +1,37 @@
+#include "include/init_configs.h"
+#include "include/motor.h"
+#include "include/pins.h"
+
+#include "include/ultrasonic_sensor.h"
+
+
+
+void setup() {
+    Serial.begin(9600);
+    cli();
+    init_pcint2_interrupt();
+    init_timer1_phase_correct_pwm();
+    start_pins();
+    sei();
+}
+
+
+
+
+void loop() {
+    unsigned int distance = 0;
+    trigger();
+    if (actual_state == PULSE_RECEIVED){
+        cli();
+        distance = 0.01723 * pulse_time_calc();
+        sei();
+        
+        Serial.println(distance);
+    }
+
+}
+
+
+
+
+
---
+++ src/motor.cpp
@@ -0,0 +1,25 @@
+#include "../include/motor.h"
+#include "../include/pins.h"
+
+
+void change_motor_state(MotorFSM_t state){
+    switch (state) {
+        case CLOCK_WISE:
+          PORTB |= (1 << IN1_AND_IN3);
+          PORTB &= ~(1 << IN2_AND_IN4);
+          break;
+        case COUNTER_CLOCK_WISE:
+          PORTB |= (1 << IN2_AND_IN4);
+          PORTB &= ~(1 << IN1_AND_IN3);
+          break;
+        case STOP:
+          PORTB &= ~((1 << IN1_AND_IN3) | (1 << IN2_AND_IN4));
+          break;
+        case BRAKE:
+          PORTB |= (1 << IN1_AND_IN3) | (1 << IN2_AND_IN4);
+        default:
+          PORTB &= ~((1 << IN1_AND_IN3) | (1 << IN2_AND_IN4));
+          break;
+    }
+
+}
