acd44996e2010c9f996229ea50f8e2616b60e68a
dratsuni <matherthentic@gmail.com>
2025-12-12 19:48:56 -0300

    driver do sensor ultrassonico corrigido. Disparo sequencial adicionado.

--- src/ultrasonic_sensor.cpp
+++ src/ultrasonic_sensor.cpp
@@ -4,31 +4,42 @@
 
 static volatile unsigned long initial_time = 0;
 static volatile unsigned long end_time = 0;
-volatile UltrasonicSensorFMS_t actual_state = PULSE_NOT_SEND;
-volatile UltrasonicSensorPosition_t actual_position;
+volatile UltrasonicSensorFSM_t actual_state = PULSE_NOT_SEND;
+volatile UltrasonicSensorPosition_t actual_position = FRONT;
 static  unsigned long latest_trigger = 0;
 
 static uint8_t ultrasonic_trig_array[ULTRASONIC_QUANTITY] = {(1 << ULTRA_SENSOR_1_TRIG), (1 << ULTRA_SENSOR_2_TRIG)};
 static uint8_t ultrasonic_echo_array[ULTRASONIC_QUANTITY] = {(1 << ULTRA_SENSOR_1_ECHO), (1 << ULTRA_SENSOR_2_ECHO)};
 static volatile uint8_t actual_sensor_index = 0;
 
-static inline void timeout(){
+
+
+static inline void update_position(){
+  if (actual_position < (ULTRASONIC_QUANTITY - 1)){
+    actual_position = (UltrasonicSensorPosition_t) (actual_position + 1);
+  } else {
+    actual_position = (UltrasonicSensorPosition_t) 0;
+
+  }
+}
+
+static inline void pulse_delay(){
    if (millis() - latest_trigger >= 60){
        actual_state = PULSE_NOT_SEND;
-        actual_sensor_index = (actual_sensor_index < (ULTRASONIC_QUANTITY - 1))?  actual_sensor_index + 1 : 0;
+       update_position();
    }
 }
+
 
 void trigger(){
     if (actual_state == PULSE_NOT_SEND || actual_state == TIMEOUT){
         latest_trigger = millis();
         actual_state = PULSE_SENT;
-        actual_position = (UltrasonicSensorPosition_t) actual_sensor_index;
-        PORTC |= ultrasonic_trig_array[actual_sensor_index];
+        PORTC |= ultrasonic_trig_array[actual_position];
         delayMicroseconds(10);
-        PORTC &= ~(ultrasonic_trig_array[actual_sensor_index]);
+        PORTC &= ~(ultrasonic_trig_array[actual_position]);
     } else {
-      timeout();
+      pulse_delay();
     }
 }
 
@@ -37,14 +48,16 @@ unsigned int pulse_time_calc(){
     return end_time - initial_time;
 }
 
+
+
 ISR (PCINT2_vect){
   if (actual_state == PULSE_SENT){
-    if ((PIND & ultrasonic_echo_array[actual_sensor_index])){
+    if ((PIND & ultrasonic_echo_array[actual_position])){
        initial_time = micros();      
     } else {
         end_time = micros();
         actual_state = PULSE_RECEIVED;
-        actual_sensor_index = (actual_sensor_index < (ULTRASONIC_QUANTITY - 1))?  actual_sensor_index + 1 : 0;
+
     }
   }
 }
