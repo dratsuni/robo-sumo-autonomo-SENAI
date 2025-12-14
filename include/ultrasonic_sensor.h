603467a4d3a3b4db8babdc4d28835b7730d3d930
dratsuni <241861453+dratsuni@users.noreply.github.com>
2025-12-14 14:53:00 -0300

    driver do sensor ultrassonico modificado. Disparo sequencial foi removido e houve melhorias na logica de disparo.

--- include/ultrasonic_sensor.h
+++ include/ultrasonic_sensor.h
@@ -1,24 +1,28 @@
 #ifndef ULTRASONIC_SENSOR_H
 #define ULTRASONIC_SENSOR_H
 #include <Arduino.h>
+#include "pins.h"
 typedef enum {
   PULSE_SENT,
   PULSE_WAIT,
   PULSE_RECEIVED,
   PULSE_NOT_SEND,
-  TIMEOUT
 
 } __attribute__((packed)) UltrasonicSensorFSM_t;
 
 typedef  enum {
-    FRONT,
-    REAR
-} __attribute__((packed)) UltrasonicSensorPosition_t;
+    FRONT = (1 << ULTRA_SENSOR_1_TRIG),
+    REAR = (1 << ULTRA_SENSOR_2_TRIG)
+} __attribute__((packed)) UltrasonicTrigPosition_t;
 
+
+typedef enum{
+  FRONT_1 = (1 << ULTRA_SENSOR_1_ECHO),
+  REAR_2 = (1 << ULTRA_SENSOR_2_ECHO)
+} __attribute__((packed)) UltrasonicEchoPosition_t;
 
 extern volatile UltrasonicSensorFSM_t actual_state;
-extern volatile UltrasonicSensorPosition_t actual_position;
 
-void trigger();
+void trigger(UltrasonicTrigPosition_t sensor_trig_pin_position, UltrasonicEchoPosition_t sensor_echo_pin_position);
 unsigned int pulse_time_calc();
 #endif
