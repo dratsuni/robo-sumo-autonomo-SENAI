d49a1dbbc22a9a756688274394ace9e9f8d35cfe
dratsuni <241861453+dratsuni@users.noreply.github.com>
2025-12-15 18:06:53 -0300

    .

--- src/ultrasonic_sensor.cpp
+++ src/ultrasonic_sensor.cpp
@@ -2,7 +2,7 @@
 #include "../include/ultrasonic_sensor.h"
 
 
-UltrasonicSensor_t ultrasonic_sensor[2] = {{FRONT_TRIG, FRONT_ECHO, {0}}, {REAR_TRIG, REAR_ECHO, {0}}}
+UltrasonicSensor_t g_ultrasonic_sensor[2] = {{FRONT_TRIG, FRONT_ECHO, {0}}, {REAR_TRIG, REAR_ECHO, {0}}};
 static volatile unsigned long initial_time = 0;
 static volatile unsigned long end_time = 0;
 
@@ -10,31 +10,31 @@ static  unsigned long latest_trigger = 0;
 static volatile unsigned long pulse_received_time = 0;
 
 static volatile UltrasonicSensorEcho_t echo_pin_position = FRONT_ECHO;
-volatile UltrasonicSensorFSM_t actual_state = PULSE_NOT_SEND;
+volatile UltrasonicSensorFSM_t g_actual_state = PULSE_NOT_SEND;
 
 __attribute__((always_inline))
 static inline void trigger_delay(){
-  if (actual_state == PULSE_RECEIVED){
+  if (g_actual_state == PULSE_RECEIVED){
       cli();
       unsigned atomic_pulse_received_time = millis() - pulse_received_time;
       sei();
       if (atomic_pulse_received_time >= 25){
-        actual_state = PULSE_NOT_SEND;
+        g_actual_state = PULSE_NOT_SEND;
       }
-  } else if (millis() - latest_trigger >= 60 && actual_state == PULSE_WAIT){
-         actual_state = TIMEOUT;
+  } else if (millis() - latest_trigger >= 60 && g_actual_state == PULSE_WAIT){
+         g_actual_state = TIMEOUT;
    }
 }
 
 
 void trigger(UltrasonicSensor_t *ultrasonic_sensor){
-    if (actual_state == PULSE_NOT_SEND || actual_state == TIMEOUT){
+    if (g_actual_state == PULSE_NOT_SEND || g_actual_state == TIMEOUT){
         latest_trigger = millis();
         PORTC |= ultrasonic_sensor->trig_pin;
         delayMicroseconds(10);
         PORTC &= ~(ultrasonic_sensor->trig_pin);
         echo_pin_position = ultrasonic_sensor->echo_pin;
-        actual_state = PULSE_SENT;
+        g_actual_state = PULSE_SENT;
     } else {  
       trigger_delay();
     }
@@ -51,14 +51,14 @@ unsigned int pulse_time_calc(){
 
 
 ISR (PCINT2_vect){
-  if (actual_state == PULSE_SENT || actual_state == PULSE_WAIT){
+  if (g_actual_state == PULSE_SENT || g_actual_state == PULSE_WAIT){
     if ((PIND & echo_pin_position)){
        initial_time = micros();     
-       actual_state = PULSE_WAIT;
+       g_actual_state = PULSE_WAIT;
     } else {
         end_time = micros();
         pulse_received_time = millis();
-        actual_state = PULSE_RECEIVED;
+        g_actual_state = PULSE_RECEIVED;
     }
   }
 }
